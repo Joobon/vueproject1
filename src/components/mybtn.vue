7889d55ff14fcfd8d44733e7f7726e5b9e897e5c
jobon <Jobon668@163.com>
2019-12-12 15:33:49 +0800

    heia

---
+++ src/components/mybtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="btn" @click="handerbtn">{{text}}</div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  methods: {
+    handerbtn (event) {
+      this.$emit('click', event)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.btn{
+    background:red;
+    width: 314*100vw/360;
+    height: 50*100vw/360;
+    line-height: 50*100vw/360;
+    border-radius:25*100vw/360;
+    text-align: center;
+    font-size: 20*100vw/360
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="close">
+        <span class="iconfont iconicon-test"></span>
+      </div>
+      <div class="logo">
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="inputs">
+        <input data-v-744880be placeholder="请输入手机号" class="input" />
+        <input data-v-744880be placeholder="密码" class="input" type="password" />
+      </div>
+      <p class="tips">
+        没有账号？
+        <a href="#/register" class>去注册</a>
+      </p>
+      <mybtn text="登录" @click="handerbtn"></mybtn>
+    </div>
+  </div>
+</template>
+
+<script>
+import mybtn from '@/components/mybtn'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    mybtn
+  },
+  methods: {
+    handerbtn (data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.container {
+  padding: 20px;
+}
+
+.close {
+  span {
+    font-size: 27 / 360 * 100vw;
+  }
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+
+  span {
+    display: block;
+    font-size: 126 / 360 * 100vw;
+    color: #d81e06;
+  }
+}
+
+.inputs {
+  input {
+    margin-bottom: 20px;
+  }
+}
+
+.tips {
+  text-align: right;
+  margin-bottom: 20px;
+
+  a {
+    color: #3385ff;
+  }
+}
+</style>
